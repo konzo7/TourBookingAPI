6b9f8dab68d5dde546db850446f16927b1cbb754
c.maiza <carlos.maiza@hotmail.com>
2021-02-04 22:31:35 +0100

    Heroku init

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 8091;
 
 app.get('/', (req, res, next) => {
-    res.send({some: "Tour Booking API"});
+    res.send({some: "This is a Tour Booking API"});
 })
 
 app.listen(port, () => { console.log(`Server listening on port ${port}`); });
